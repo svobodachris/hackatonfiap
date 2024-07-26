d9e0d8400084c4695f862a0be823a49f2bd920dd
svobodachris <147349677+svobodachris@users.noreply.github.com>
2024-07-26 18:21:08 -0300

    Swagger Config

---
+++ src/config/swaggerConfig.js
@@ -0,0 +1,48 @@
+// config/swaggerConfig.js
+
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Health&Med API',
+      version: '1.0.0',
+      description: 'API para a aplicação Health&Med',
+    },
+    //
+    components: {
+        schemas: {
+          Appointment: {
+            type: 'object',
+            required: ['doctorId', 'patientId', 'date', 'time'],
+            properties: {
+              doctorId: {
+                type: 'string',
+                description: 'ID do médico',
+              },
+              patientId: {
+                type: 'string',
+                description: 'ID do paciente',
+              },
+              date: {
+                type: 'string',
+                format: 'date',
+                description: 'Data da consulta',
+              },
+              time: {
+                type: 'string',
+                format: 'time',
+                description: 'Hora da consulta',
+              },
+            },
+          },
+        },
+      },
+    },
+  apis: ['./src/routes/*.js'], // Caminho para os arquivos de rotas
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
